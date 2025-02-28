b5f9aec92576c640d5a31a5ff58b0fc90352a720
Haryodh Singh <233260@student.wqe.ac.uk>
2025-02-28 11:31:54 +0000

    aaa

--- TestingChess/Form1.Designer.cs
+++ TestingChess/Form1.Designer.cs
@@ -28,34 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Location = new System.Drawing.Point(256, 96);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(460, 254);
-            this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- TestingChess/Form1.cs
+++ TestingChess/Form1.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TestingChess
+namespace TestingChess //MAKE SURE THIS IS CHANGED TO YOUR PROJECT NAME DUMMY
 {
     public partial class Form1 : Form
     {
-        Button[,] buttons = new Button[8, 8];
+        //REMEMBER TO COPY THESE SILLY BOY
+        Panel panel1 = new Panel();
+        Button[,] buttons = new Button[2, 8];
         public Form1()
         {
             InitializeComponent();
@@ -20,15 +22,19 @@ namespace TestingChess
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            this.Resize += Form1_Resize;
+            panel1.BackColor = Color.Black;
+            this.Controls.Add(panel1);
          
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < buttons.GetLength(0); i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < buttons.GetLength(1); j++)
                 {
                     buttons[i,j] = new Button();
                     panel1.Controls.Add(buttons[i, j]);
-                    buttons[i,j].Size=new Size(buttons[i,j].Parent.Width/8, buttons[i, j].Parent.Height / 8);
-                    buttons[i,j].Location = new Point((buttons[i, j].Parent.Width/8)*j, (buttons[i, j].Parent.Height / 8) * i);
+                    buttons[i,j].Size=new Size(buttons[i,j].Parent.Width/ buttons.GetLength(1), buttons[i, j].Parent.Height / buttons.GetLength(0));
+                    buttons[i,j].Location = new Point((buttons[i, j].Parent.Width/ buttons.GetLength(1)) *j, (buttons[i, j].Parent.Height / buttons.GetLength(0)) * i);
                     buttons[i,j].FlatStyle = FlatStyle.Flat;
                     buttons[i,j].BackColor = Color.White;
                     buttons[i, j].Click += on_click;
@@ -60,12 +66,12 @@ namespace TestingChess
             panel1.Size = new Size(scaledwidth, scaledheight);
             panel1.BackColor = Color.Black;
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < buttons.GetLength(0); i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < buttons.GetLength(1); j++)
                 {
-                    buttons[i, j].Size = new Size(buttons[i, j].Parent.Width / 8, buttons[i, j].Parent.Height / 8);
-                    buttons[i, j].Location = new Point((buttons[i, j].Parent.Width / 8) * j, (buttons[i, j].Parent.Height / 8) * i);
+                    buttons[i, j].Size = new Size(buttons[i, j].Parent.Width / buttons.GetLength(1), buttons[i, j].Parent.Height / buttons.GetLength(0));
+                    buttons[i, j].Location = new Point((buttons[i, j].Parent.Width / buttons.GetLength(1)) * j, (buttons[i, j].Parent.Height / buttons.GetLength(0)) * i);
                 }
             }
         }
